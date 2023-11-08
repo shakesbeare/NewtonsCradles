74d2f086926d49177441cf14df27d8316bb84fb3
Berint Moffett <berint.moffett@gmail.com>
2023-11-08 15:14:27 -0700

    PhysicsBody implementation

--- ball.gd
+++ ball.gd
@@ -1,13 +1,5 @@
-extends RigidBody2D
+class_name Ball
+extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
-
-
+    self.linear_velocity = Vector2(-500, 0)
--- rope.gd
+++ rope.gd
@@ -1,22 +1,3 @@
 class_name Rope
-extends Sprite2D
-
-signal rope_length(float)
-
-const sprite_height: float = 64
-
-@export var length: float = 2
-@export var ball: Node2D
-@export var bottom_connector: Node2D
-
-func _ready():
-    self.scale = Vector2(1, length)
-    ball.scale = Vector2(1, 1 / length)
-
-    self.position.y = sprite_height * length / 2
-    bottom_connector.position.y = sprite_height * length / 2
-    ball.position.y = 32
-
-    rope_length.emit(length)
-
+extends RigidBody2D
 
